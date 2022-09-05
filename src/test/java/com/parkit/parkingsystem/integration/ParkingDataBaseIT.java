162dc966471216e33de211e20a3f8fe6342b4ffe
HashTucE <jeremyb59@wanadoo.fr>
2022-09-05 18:44:12 +0200

    Corrections tests intégration ParkingDataBaseIT

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -4,15 +4,16 @@ import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.ParkingSpot;
+import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -21,13 +22,15 @@ public class ParkingDataBaseIT {
     private static DataBaseTestConfig dataBaseTestConfig = new DataBaseTestConfig();
     private static ParkingSpotDAO parkingSpotDAO;
     private static TicketDAO ticketDAO;
+
+    private Ticket ticket = new Ticket();
     private static DataBasePrepareService dataBasePrepareService;
 
     @Mock
     private static InputReaderUtil inputReaderUtil;
 
     @BeforeAll
-    private static void setUp() throws Exception{
+    private static void setUp() throws Exception {
         parkingSpotDAO = new ParkingSpotDAO();
         parkingSpotDAO.dataBaseConfig = dataBaseTestConfig;
         ticketDAO = new TicketDAO();
@@ -43,23 +46,56 @@ public class ParkingDataBaseIT {
     }
 
     @AfterAll
-    private static void tearDown(){
+    private static void tearDown() {
 
     }
 
     @Test
-    public void testParkingACar(){
+    @DisplayName("Check that a ticket is saved in DB and the parking table is updated")
+    public void testParkingACar() throws Exception {
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
-        //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
+
+        // DONE: check that a ticket is actually saved in DB
+        ticket = ticketDAO.getTicket("ABCDEF");
+        assertNotNull(ticket);
+
+        // DONE: check that the Parking table is updated with availability
+        ParkingSpot parkingSpot = ticket.getParkingSpot();
+        int parkingNumber = parkingSpot.getId();
+        assertEquals(1, parkingNumber);
+        assertFalse(parkingSpot.isAvailable());
     }
 
     @Test
-    public void testParkingLotExit(){
+    @DisplayName("Check that the fare and the timeout are generated correctly in the DB")
+    public void testParkingLotExit() throws Exception {
         testParkingACar();
+        Thread.sleep(1000);
+
+        when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processExitingVehicle();
-        //TODO: check that the fare generated and out time are populated correctly in the database
+        ticket = ticketDAO.getTicket("ABCDEF");
+//		ticket.setInTime(new Date(System.currentTimeMillis() - (60 * 60 * 1000)));
+
+
+        // DONE: check that the fare are generated correctly in the database
+        assertEquals(0, ticket.getPrice());
+
+        // DONE: check that out time are populated correctly in the database
+        assertNotNull(ticket.getOutTime());
+        assertNotEquals(ticket.getInTime(), ticket.getOutTime());
+
+
+//		System.out.println("------------------------------------------");
+//		System.out.println("Date entrée: " + ticket.getInTime());
+//		System.out.println("Date sortie: " + ticket.getOutTime());
+//		System.out.println("Prix: " + ticket.getPrice());
+//		System.out.println("------------------------------------------");
+
+
+
     }
 
 }
