7a65df12984c603f5f35d3c59a798735c95d4bf7
HashTucE <jeremyb59@wanadoo.fr>
2022-09-05 18:37:43 +0200

    Ajout tests unitaires ParkingSpotDAOTest

---
+++ src/test/java/com/parkit/parkingsystem/dao/ParkingSpotDAOTest.java
@@ -0,0 +1,79 @@
+package com.parkit.parkingsystem.dao;
+
+import com.parkit.parkingsystem.constants.ParkingType;
+import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.ParkingSpot;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParkingSpotDAOTest {
+
+    private static ParkingSpotDAO parkingSpotDAO = new ParkingSpotDAO();
+
+    private static DataBasePrepareService dataBasePrepareService;
+
+    @BeforeAll
+    private static void setUp() throws Exception {
+        dataBasePrepareService = new DataBasePrepareService();
+    }
+
+    @BeforeEach
+    private void setUpPerTest() throws Exception {
+        dataBasePrepareService.clearDataBaseEntries();
+    }
+
+
+    @Test
+    @DisplayName("Should return the next available slot when there is a slot available")
+    void getNextAvailableSlotWhenThereIsASlotAvailable() {
+        try {
+            int nextAvailableSlot = parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR);
+
+            assertEquals(1, nextAvailableSlot);
+        } catch (Exception e) {
+            fail("Exception should not be thrown");
+        }
+    }
+
+    @Test
+    @DisplayName("Should return 0 when there is no car slot available")
+    void getNextAvailableSlotWhenThereIsNoSlotAvailable() {
+
+        parkingSpotDAO = new ParkingSpotDAO();
+        ParkingSpot ps1 = new ParkingSpot(1, ParkingType.CAR, false);
+        parkingSpotDAO.updateParking(ps1);
+        ParkingSpot ps2 = new ParkingSpot(2, ParkingType.CAR, false);
+        parkingSpotDAO.updateParking(ps2);
+        ParkingSpot ps3 = new ParkingSpot(3, ParkingType.CAR, false);
+        parkingSpotDAO.updateParking(ps3);
+
+
+        int result = parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR);
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    @DisplayName("Should return true when the parking spot is updated")
+    void updateParkingWhenParkingSpotIsUpdatedThenReturnTrue() {
+
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR, false);
+        boolean result = parkingSpotDAO.updateParking(parkingSpot);
+
+        assertTrue(result);
+    }
+
+    @Test
+    @DisplayName("Should return false when the parking spot is not updated")
+    void updateParkingWhenParkingSpotIsNotUpdatedThenReturnFalse() {
+
+        ParkingSpot parkingSpotUnknown = new ParkingSpot(0, ParkingType.CAR, false);
+        boolean result = parkingSpotDAO.updateParking(parkingSpotUnknown);
+
+        assertFalse(result);
+    }
+}
