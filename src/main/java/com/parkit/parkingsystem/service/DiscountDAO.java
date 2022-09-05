b68504509663dce09d4dd35823c18fe59ff07f23
HashTucE <jeremyb59@wanadoo.fr>
2022-09-05 18:08:50 +0200

    Ajoute fonctionnalité 5% de fidélité

---
+++ src/main/java/com/parkit/parkingsystem/service/DiscountDAO.java
@@ -0,0 +1,29 @@
+package com.parkit.parkingsystem.service;
+
+import com.parkit.parkingsystem.dao.TicketDAO;
+import com.parkit.parkingsystem.model.Ticket;
+
+public class DiscountDAO {
+
+    private TicketDAO ticketDAO = new TicketDAO();
+
+    public DiscountDAO() {
+    }
+
+    public DiscountDAO(TicketDAO ticketDAO) {
+        this.ticketDAO = ticketDAO;
+    }
+
+    public boolean discount(String incomingRegistration) {
+
+        boolean result = false;
+
+        Ticket ticket = ticketDAO.getTicket(incomingRegistration);
+
+        if (ticket != null) {
+            result = true;
+        }
+
+        return result;
+    }
+}
--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -7,9 +7,25 @@ import java.time.Duration;
 
 public class FareCalculatorService {
 
-    public void calculateFare(Ticket ticket){
-        if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
-            throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
+    private DiscountDAO discountDAO;
+
+    public DiscountDAO getDiscountDAO() {
+        return discountDAO;
+    }
+
+    public void setDiscountDAO(DiscountDAO discountDAO) {
+        this.discountDAO = discountDAO;
+    }
+
+    public FareCalculatorService() {
+        this.discountDAO = new DiscountDAO();
+    }
+    public void calculateFare(Ticket ticket) throws Exception {
+
+        boolean result;
+
+        if ((ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime()))) {
+            throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());
         }
 
         Duration duration = Duration.between(ticket.getInTime().toInstant(), ticket.getOutTime().toInstant());
@@ -20,16 +36,22 @@ public class FareCalculatorService {
             ticket.setPrice(Fare.FREE_PARKING);
         } else {
 
-        switch (ticket.getParkingSpot().getParkingType()){
-            case CAR: {
-                ticket.setPrice(durationToHours * Fare.CAR_RATE_PER_HOUR);
-                break;
+            switch (ticket.getParkingSpot().getParkingType()) {
+                case CAR: {
+                    ticket.setPrice(durationToHours * Fare.CAR_RATE_PER_HOUR);
+                    break;
+                }
+                case BIKE: {
+                    ticket.setPrice(durationToHours * Fare.BIKE_RATE_PER_HOUR);
+                    break;
+                }
+                default:
+                    throw new IllegalArgumentException("Unkown Parking Type");
             }
-            case BIKE: {
-                ticket.setPrice(durationToHours * Fare.BIKE_RATE_PER_HOUR);
-                break;
+            result = discountDAO.discount(ticket.getVehicleRegNumber());
+
+            if (result)
+                ticket.setPrice(ticket.getPrice() * 0.95);
             }
-            default: throw new IllegalArgumentException("Unkown Parking Type");
         }
     }
-}
